6be354165411135055b6885df298c59e1e8e8f56
loong <43443038+realoong@users.noreply.github.com>
2024-10-30 10:38:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,14 +20,14 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个干饭人,欢迎来到我的博客！🎉',
+    'Hi，这里是精进系统,欢迎来到我的博客！🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Loonge', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人 🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://loonge.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '龙哥, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '精进, 龙哥, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   //CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '360888122@qq.com', // 邮箱地址 例如mail@tangly1024.com
