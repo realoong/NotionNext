596d5589fe80c6c0acd93179dada8232f72ff89a
loong <43443038+realoong@users.noreply.github.com>
2024-10-30 11:37:13 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,12 +18,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_1: '终身学习',
+  HEO_HERO_TITLE_2: '持续精进',
   HEO_HERO_TITLE_3: 'LOONGE.TOP',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://www.loonge.top',
+  HEO_HERO_TITLE_4: '人生精进系统',
+  HEO_HERO_TITLE_5: '一件未来20年都受益匪浅的事',
+  HEO_HERO_TITLE_LINK: 'https://www.loonge.top/article/RefinementSystem',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
