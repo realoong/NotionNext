5b2f7a317c3f35ec69faba64f343f1563b8b6624
loong <43443038+realoong@users.noreply.github.com>
2024-10-31 14:53:30 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -53,7 +53,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/realoong',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.loonge.top',
+  HEO_INFO_CARD_URL3: 'https://www.loonge.top/about',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -118,8 +118,8 @@ const CONFIG = {
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
+  HEO_SOCIAL_CARD_TITLE_2: '欢迎与我讨论分享',
+  HEO_SOCIAL_CARD_TITLE_3: '点击了解更多',
   HEO_SOCIAL_CARD_URL: 'https://www.loonge.top/about',
 
   // *****  以下配置无效，只是预留开发 ****
