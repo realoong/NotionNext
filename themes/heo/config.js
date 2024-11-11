006e0b3dd914b754e41de0e42d0b873f47587244
loong <43443038+realoong@users.noreply.github.com>
2024-11-11 17:02:01 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -30,7 +30,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '身心健康', url: '/category/身心健康' },
   HEO_HERO_CATEGORY_2: { title: '财富积累', url: '/category/财富积累' },
-  HEO_HERO_CATEGORY_3: { title: '社交艺术', url: '/category/社交艺术' },
+  HEO_HERO_CATEGORY_3: { title: '社交智慧', url: '/category/社交智慧' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
